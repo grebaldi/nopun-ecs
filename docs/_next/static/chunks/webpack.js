6841762cb8795f1cf32ea57bdee958991c3d19c3
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2021-05-11 23:29:52 +0200

    Add github ribbon

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2b17bef33c0217dfd6b2";
+/******/ 	var hotCurrentHash = "3fbfea688cd9e5a2509a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
